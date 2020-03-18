87da1e9be8bc854d3334ba5333a22045666f2aa2
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 18:41:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
-  attr_accessor :name
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(name)
-      @name = name
+  def initialize(glove_size)
+      @glove_size = glove_size
   end
 end
