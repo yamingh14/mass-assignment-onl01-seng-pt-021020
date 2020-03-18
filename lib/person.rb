1a36ee3700d4a9d9e73c100215ae813f5e697c66
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 18:42:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,11 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
+                :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(glove_size)
-      @glove_size = glove_size
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
   end
 end
