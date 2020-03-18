7a6d5a5c6235790874d6bb81a231415becde852b
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-18 17:46:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(name, birthday, hair_color, eye_color, height, weight, handed, complexion, t_shirt_size, wrist_size, glove_size, pant_length, pant_width)
+  @name = name
+  @birthday = birthday
+  @hair_color = hair_color
+  @eye_color = eye_color
+  @height = height
+  @weight = weight
+  @handed = handed
+  @complexion = complexion
+  @t_shirt_size = t_shirt_size
+  @wrist_size = wrist_size
+  @glove_size = glove_size
+  @pant_length = pant_length
+  @pant_width = pant_width
 end
